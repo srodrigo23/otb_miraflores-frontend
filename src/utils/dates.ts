3c144f052988f5a835db72c6362d3a7f8d43a151
rodres101 <rodrigosergio93@gmail.com>
2026-04-26 19:38:47 -0400

    form new measure

---
+++ src/utils/dates.ts
@@ -0,0 +1,9 @@
+
+// Obtener la fecha actual en formato YYYY-MM-DD para el campo de fecha
+export const getTodayDate = () => {
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = String(today.getMonth() + 1).padStart(2, '0');
+  const day = String(today.getDate()).padStart(2, '0');
+  return `${year}-${month}-${day}`;
+};
