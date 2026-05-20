c706e661975138f446b00e2d3da4195448551ef5
rodres101 <rodrigosergio93@gmail.com>
2026-05-20 18:37:08 -0400

    improve layout

--- src/App.tsx
+++ src/App.tsx
@@ -31,56 +31,13 @@ function App() {
         pauseOnHover
         theme='light'
       />
-      <div
-      // className='h-dvh flex flex-col lg:grid lg:grid-cols-12 bg-gray-50'
-      >
-        {/* Mobile Header */}
-        {/* <div className="lg:hidden bg-gradient-to-r from-blue-600 to-blue-800 p-4 flex items-center justify-between shadow-lg z-20">
-          <h1 className="text-white font-bold text-lg">OTB MIRAFLORES</h1>
-          <button
-            onClick={toggleSidebar}
-            className="text-white p-2 hover:bg-blue-700 rounded-lg transition-colors"
-          >
-            {isSidebarOpen ? <XCircle size={28} weight="bold" /> : <ListBullets size={28} weight="bold" />}
-          </button>
-        </div> */}
-
-        {/* Overlay para móvil */}
-        {/* {isSidebarOpen && (
-          <div
-            className="lg:hidden fixed inset-0 bg-black bg-opacity-50 z-30"
-            onClick={toggleSidebar}
-          />
-        )} */}
-
-        {/* Sidebar */}
-        {/* <div
-          className={`
-            fixed lg:relative lg:col-span-3 xl:col-span-2 h-dvh
-            transform transition-transform duration-300 ease-in-out z-40
-            ${isSidebarOpen ? 'translate-x-0' : '-translate-x-full lg:translate-x-0'}
-            w-80 lg:w-full
-          `}
-        >
-          <div className="h-full overflow-y-auto">
-            <NavBarComponent pathName={pathname}/>
-          </div>
-        </div> */}
-
-        {/* Main Content */}
-
-        <div
-        // className="lg:col-span-9 xl:col-span-10 flex-1 overflow-y-auto bg-white lg:border-l lg:shadow-inner"
-        // className='h-screen'
-          className='flex flex-col'
-        >
-          <TopNavBar pathName={pathname} />
-          <div className='mx-auto container pb-5'>
-            <Outlet />
-          </div>
-          <div className='bg-black h-10 text-white flex justify-center items-center'>
-            <Typography variant='h5'>Kratos Software</Typography>
-          </div>
+      <div className='lg:h-screen flex flex-col'>
+        <TopNavBar pathName={pathname} />
+        <div className='mx-auto container w-full pb-5 lg:flex-1 lg:min-h-0'>
+          <Outlet />
+        </div>
+        <div className='bg-black h-10 text-white flex justify-center items-center'>
+          <Typography variant='h5'>Kratos Software</Typography>
         </div>
       </div>
     </>
--- src/pages/neighbors/Neighbors.tsx
+++ src/pages/neighbors/Neighbors.tsx
@@ -23,8 +23,8 @@ const Neighbors = () => {
       {loading ? (
         <LoaderAnimation isLoading={loading} />
       ) : (
-        <div className='flex flex-col gap-6 px-4 py-4'>
-          <div className='flex flex-col sm:flex-row justify-between items-start sm:items-center gap-4'>
+        <div className='flex flex-col gap-6 px-4 py-4 lg:h-full lg:overflow-hidden'>
+          <div className='flex flex-col sm:flex-row justify-between items-start sm:items-center gap-4 flex-shrink-0'>
             <Typography variant='h2'>Detalle General de Vecinos</Typography>
             <Button className='w-full sm:w-auto'>NUEVO VECINO</Button>
           </div>
@@ -39,12 +39,12 @@ const Neighbors = () => {
             />
           </div>
 
-          <div className='flex flex-col gap-4'>
-            <div className='flex justify-center'>
+          <div className='flex flex-col gap-4 lg:flex-1 lg:overflow-y-auto lg:min-h-0'>
+            <div className='flex justify-center flex-shrink-0'>
               <EditNeighborInfoForm />
             </div>
 
-            <div className='border rounded-xl'>
+            <div className='border rounded-xl lg:flex-1 lg:min-h-0'>
               <NeighborDebtsPayments />
             </div>
           </div>
