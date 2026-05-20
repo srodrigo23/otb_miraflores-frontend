2e25e34e14131e38ef967c19830090fe4f3f02c2
rodres101 <rodrigosergio93@gmail.com>
2026-05-20 15:08:17 -0400

    neighborlist filtered

--- src/components/lists/NeighborsList.tsx
+++ src/components/lists/NeighborsList.tsx
@@ -1,3 +1,4 @@
+import { useMemo, useState } from "react";
 import { Input, Card, List, ListItem, Typography, Chip } from "@material-tailwind/react";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { NeighborType } from "../../interfaces/neighborsInterfaces";
@@ -5,25 +6,35 @@ import { NeighborType } from "../../interfaces/neighborsInterfaces";
 export const NeighborList: React.FC<{ neighborsData: NeighborType[] }> = ({
   neighborsData,
 }) => {
+
+  const [searchTerm, setSearchTerm] = useState<string>('');
+  
+  const filteredData = useMemo(()=>{
+    return neighborsData.filter((neigbor)=>{
+      const fullName = `${neigbor.first_name} ${neigbor.second_name} ${neigbor.last_name} `;
+      return fullName.includes(searchTerm.toLocaleLowerCase())
+    })
+  }, [searchTerm, neighborsData])
+
   return (
     <div className='w-1/3 border rounded-md'>
       <div className='flex pt-4 px-4 pb-1'>
         <Input
           label='Buscar ...'
           icon={<MagnifyingGlassIcon className='h-5 w-5' />}
-          // value={searchTerm}
-          // onChange={(e) => setSearchTerm(e.target.value)}
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
           crossOrigin={undefined}
         />
       </div>
 
       <Card className='overflow-y-auto h-screen'>
         <List className='gap-0'>
-          {neighborsData.map((neighbor, index) => {
+          {filteredData.map((neighbor, index) => {
             return (
               <ListItem
                 className='flex justify-between items-center border-t py-1'
-                selected={index == 0}
+                // selected={index == 0}
               >
                 <p className='flex gap-4 items-center'>
                   <span className='text-sm'>{index + 1}</span>
