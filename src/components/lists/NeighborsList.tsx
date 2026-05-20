8f38b2f45096095e79697d0f743e1a23bb6a5b5e
rodres101 <rodrigosergio93@gmail.com>
2026-05-20 17:32:54 -0400

    improvement neighborlist

--- src/components/lists/NeighborsList.tsx
+++ src/components/lists/NeighborsList.tsx
@@ -1,6 +1,6 @@
-import { useMemo, useState } from "react";
-import { Input, Card, List, ListItem, Typography, Chip } from "@material-tailwind/react";
-import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { useEffect, useMemo, useState } from "react";
+import { Input, Card, List, ListItem, Typography, Chip, IconButton } from "@material-tailwind/react";
+import { MagnifyingGlassIcon, XMarkIcon} from "@heroicons/react/24/outline";
 import { NeighborType } from "../../interfaces/neighborsInterfaces";
 
 export const NeighborList: React.FC<{ neighborsData: NeighborType[] }> = ({
@@ -11,14 +11,28 @@ export const NeighborList: React.FC<{ neighborsData: NeighborType[] }> = ({
   
   const filteredData = useMemo(()=>{
     return neighborsData.filter((neigbor)=>{
-      const fullName = `${neigbor.first_name} ${neigbor.second_name} ${neigbor.last_name} `;
+      const fullName = `${neigbor.first_name} ${neigbor.second_name} ${neigbor.last_name}`.toLowerCase();
       return fullName.includes(searchTerm.toLocaleLowerCase())
     })
   }, [searchTerm, neighborsData])
 
+  const [neighborSelected, setNeighborSelected] = useState<NeighborType | null>(null);
+
+  useEffect(() => {
+    // if (neighborSelected) {
+    //   const stillExists = filteredData.find(n => n.id === neighborSelected.id);
+    //   if (!stillExists) {
+    //     setNeighborSelected(filteredData[0] ?? null);
+    //   }
+    // }
+    // } else {
+    //   setNeighborSelected(filteredData[0] ?? null);
+    // }
+  }, [filteredData]);
+
   return (
     <div className='w-1/3 border rounded-md'>
-      <div className='flex pt-4 px-4 pb-1'>
+      <div className='flex gap-2 pt-4 px-4 pb-1'>
         <Input
           label='Buscar ...'
           icon={<MagnifyingGlassIcon className='h-5 w-5' />}
@@ -26,31 +40,50 @@ export const NeighborList: React.FC<{ neighborsData: NeighborType[] }> = ({
           onChange={(e) => setSearchTerm(e.target.value)}
           crossOrigin={undefined}
         />
+        {searchTerm ? (
+          <IconButton variant="gradient" color="cyan" size="md" onClick={() => setSearchTerm('')}><XMarkIcon className="w-5 h-5"/></IconButton>
+        ) : (
+          <></>
+        )}
       </div>
 
-      <Card className='overflow-y-auto h-screen'>
-        <List className='gap-0'>
-          {filteredData.map((neighbor, index) => {
-            return (
-              <ListItem
-                className='flex justify-between items-center border-t py-1'
-                // selected={index == 0}
-              >
-                <p className='flex gap-4 items-center'>
-                  <span className='text-sm'>{index + 1}</span>
-                  <Typography
-                    variant='paragraph'
-                    className='font-semibold text-sm'
+      {searchTerm ? (
+        <Card className='overflow-y-auto max-h-40 min-h-16'>
+          {filteredData.length > 0 ? (
+            <List className='gap-0'>
+              {filteredData.map((neighbor, index) => {
+                return (
+                  <ListItem
+                    className='flex justify-between items-center border-t py-1'
+                    selected={neighborSelected?.id === neighbor.id}
+                    onClick={() => {
+                      setSearchTerm('')
+                      setNeighborSelected(neighbor);
+                    }}
                   >
-                    {`${neighbor.last_name} ${neighbor.first_name} ${neighbor.second_name}`}
-                  </Typography>
-                </p>
-                <Chip value={123} color='red' />
-              </ListItem>
-            );
-          })}
-        </List>
-      </Card>
+                    <p className='flex gap-4 items-center'>
+                      <span className='text-sm'>{index + 1}</span>
+                      <Typography
+                        variant='paragraph'
+                        className='font-semibold text-sm'
+                      >
+                        {`${neighbor.last_name} ${neighbor.first_name} ${neighbor.second_name}`}
+                      </Typography>
+                    </p>
+                    <Chip value={123} color='red' />
+                  </ListItem>
+                );
+              })}
+            </List>
+          ) : (
+            <div className='flex items-center justify-center h-16'>
+              <Typography variant="h5">No existe este nombre</Typography>
+            </div>
+          )}
+        </Card>
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
